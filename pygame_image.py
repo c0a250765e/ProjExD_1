b549bbb443b9e171eba7ae65abc5e4dc610ae7bc
kikuchih <c0a250765e@edu.teu.ac.jp>
2026-06-09 17:34:24 +0900

    演習課題2

--- pygame_image.py
+++ pygame_image.py
@@ -22,16 +22,19 @@ def main():
 
         key_lst = pg.key.get_pressed()
 
-        kk_rct.move_ip(-1,0)
+        vx = -1
+        vy = 0
 
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            vy -= 1
         if key_lst[pg.K_DOWN]:  
-            kk_rct.move_ip(0, +1)  
+            vy += 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            vx -= 1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)        
+            vx += 2
+
+        kk_rct.move_ip(vx,vy)      
 
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
